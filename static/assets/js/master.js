51ff77512f0c121151167ce5b82ab128a295e313
simpleParadox <abomicode@gmail.com>
2018-01-06 10:11:30 +0530

    Added basic animations

--- static/assets/js/master.js
+++ static/assets/js/master.js
@@ -1,7 +1,41 @@
 $(document).ready(() =>{
-    console.log("Hello World!, This is Rohan."); 
+    console.log("Awesome, you found the source.\n Feel free to check it out.");
+    
+    var section1Top = parseInt($('#landing-intro').offset().top) - 50;
+    var section2Top = parseInt($('#i-like-these').offset().top) - 75;
+    var section3Top = parseInt($('#anchor3').offset().top) - 50;
+    var section1Flag = 0;
+    var section2Flag = 0;
+    var section3Flag = 0;
+    
+    // Implementing animations when reaching a page.
+    window.onscroll = function() {
+        var bodyTop = $(document).scrollTop();
+        //console.log(document.body.scrollTop);
+        if(section1Flag==0 && (bodyTop > section1Top)){
+            // Start animation
+            $('#acad-heading').fadeTo(1000,1);
+            section1Flag = 1;
+            setTimeout(()=>{
+                $('#wrapper-academics').fadeTo(800,1);
+            },650);
+        }
+        else if(section2Flag==0 && bodyTop > section2Top){
+            $('#skills .container-fluid').fadeTo(1000,1);
+            section2Flag = 1;
+        }
+        else if(section3Flag==0 && bodyTop > section3Top){
+            $('#extra .container-fluid .row').fadeTo(1000,1);
+            section3Flag = 1;
+        }
+    };
+    
+    // Sliding in
     setTimeout(()=>{
-        $('#my-name').fadeTo(1000,1);
+        $('#my-name').animate({'margin-left':'0px'},800);
+    });
+    
+    setTimeout(()=>{
         setTimeout(()=>{$('#lgi-1').fadeTo(600,1);},800);
         setTimeout(()=>{$('#lgi-2').fadeTo(600,1);},1600);
         setTimeout(()=>{$('#lgi-3').fadeTo(600,1);},2400);
@@ -9,7 +43,7 @@ $(document).ready(() =>{
         setTimeout(()=>{
             $('#down-arrow').fadeTo(500,1);
         },3100);
-    },0);
+    },500);
     // Implementing smooth scrolling.
     $('a[href^="#"]').on('click',function(event){
         event.preventDefault(); 
